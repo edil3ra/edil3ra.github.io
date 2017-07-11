dea304cfe505e02725f5ef2f127957a2843adc39
edil3ra <edil3ra@gmail.com>
2017-07-11 14:46:43 +0200

    Nikola auto commit.
    Nikola version: 7.8.5

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 	svn export https://github.com/edil3ra/pong/trunk/public games/pong --force
 	svn export https://github.com/edil3ra/life/trunk/public games/life --force
 
+
+# cv
+    pandoc /home/vince/blog/pages/cv.org -f org -t rst -o ~/tmp/cv.rst
+    https://convertfiles.online/convert/rst/odt
+    https://convertfiles.online/convert/rst/docx
+    https://convertfiles.online/convert/rst/pdf
--- themes/personal/assets/js/my.js
+++ themes/personal/assets/js/my.js
@@ -47,6 +47,41 @@ if(content) {
   })
 
   el.html(contentReplaced)
-
-
 }
+
+// add pdf odt docx links
+// it's ideal to do it in javascript so it won't messed up with org export
+var cvs_info = [
+  {
+    url: '/assets/files/cv.pdf',
+    text: 'pdf',
+    icon: 'fa fa-file-pdf-o fa-2x',
+  },
+  {
+    url: '/assets/files/cv.docx',
+    text: 'docx',
+    icon: 'fa fa-file-word-o fa-2x',
+  },
+  {
+    url: '/assets/files/cv.odt',
+    text: 'odt',
+    icon: 'fa fa-file-o fa-2x',
+  }
+]
+var cvs = ''
+cvs += '<ul>'
+for(cv of cvs_info) {
+  cvs += '<li><a href="' + cv.url + '" title="' + cv.text + ' "target="_blanck"> <i aria-hidden="true" class="' + cv.icon + '"></i> </a></li>' 
+}
+cvs += '</ul>'
+
+
+var el = $('.post-cv header')
+el.append(cvs)
+var h1 = $('.post-cv header h1')
+var ul = $('.post-cv header ul')
+var lis = $('.post-cv header ul li')
+el.addClass('my-post-cv-el')
+h1.addClass('my-post-cv-h1')
+ul.addClass('my-post-cv-ul')
+lis.addClass('my-post-cv-lis')
